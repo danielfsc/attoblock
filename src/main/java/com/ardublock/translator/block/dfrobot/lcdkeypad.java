194e8e06fd84f289b82584fee75905a8e08d95a9
danielfsc <girardi1309@gmail.com>
2021-08-16 15:21:47 -0300

    Initial windows commit

--- src/main/java/com/ardublock/translator/block/dfrobot/lcdkeypad.java
+++ src/main/java/com/ardublock/translator/block/dfrobot/lcdkeypad.java
@@ -6,24 +6,21 @@ import com.ardublock.translator.block.exception.SocketNullException;
 import com.ardublock.translator.block.exception.SubroutineNotDeclaredException;
 
 public class lcdkeypad extends TranslatorBlock {
-	
-	public lcdkeypad(Long blockId, Translator translator, String codePrefix, String codeSuffix, String label)
-	{
+
+	public lcdkeypad(Long blockId, Translator translator, String codePrefix, String codeSuffix, String label) {
 		super(blockId, translator, codePrefix, codeSuffix, label);
 	}
 
-	//@Override
-	public String toCode() throws SocketNullException, SubroutineNotDeclaredException
-	{
-		// récupération des paramètres du module, ici le message placé en rang 0
-		// on écrit donc le code Ã  générer
+	// @Override
+	public String toCode() throws SocketNullException, SubroutineNotDeclaredException {
+		//
 		TranslatorBlock translatorBlock = this.getRequiredTranslatorBlockAtSocket(0, "lcd.print( ", " );\n");
-		// création du texte de code correspondant
+		//
 		translator.addHeaderFile("LiquidCrystal.h");
 		translator.addDefinitionCommand("LiquidCrystal lcd(12, 11, 5, 4, 3, 2);");
 		translator.addSetupCommand("lcd.begin(16, 2);");
 		String ret = translatorBlock.toCode();
 		return ret;
 	}
-	
+
 }
